f6f23ce1b8de6a9963b4057394aacbb0944d1b6a
Joshua Yoes <>
2020-01-16 20:34:43 -0800

    CSS layout of buttons and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-          Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-          and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-          Learn React
-      </a>
-    </header>
-  </div>
+  <main>
+    <section id="display" />
+    <button type="button" id="equals">=</button>
+    <button type="button" id="zero">0</button>
+    <button type="button" id="one">1</button>
+    <button type="button" id="two">2</button>
+    <button type="button" id="three">3</button>
+    <button type="button" id="four">4</button>
+    <button type="button" id="five">5</button>
+    <button type="button" id="six">6</button>
+    <button type="button" id="seven">7</button>
+    <button type="button" id="eight">8</button>
+    <button type="button" id="nine">9</button>
+    <button type="button" id="decimal">.</button>
+    <button type="button" id="clear">AC</button>
+    <button type="button" id="add">+</button>
+    <button type="button" id="subtract">-</button>
+    <button type="button" id="multiply">x</button>
+    <button type="button" id="divide">/</button>
+  </main>
 );
 
 export default App;
